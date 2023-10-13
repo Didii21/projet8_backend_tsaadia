ef3c7a52a9b9783b12e9ea2c42c966a008cd4f00
traorechadii <traorechadii@gmail.com>
2023-10-13 16:18:40 +0000

    afficher_les_identifiants

---
+++ app/Models/Etudiant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use App\Models\Image;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'classe',
+        'photos',
+    ];
+    public function image()
+    {
+     return $this->hasOne(Image::class);   
+    }
+}
